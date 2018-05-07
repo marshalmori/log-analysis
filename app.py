412463dbf21d8e4ad0b20829656d8779e27c065b
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2018-05-07 13:18:30 -0300

    Criação das funções do arquivo erros.py

--- app.py
+++ app.py
@@ -9,18 +9,24 @@ Database.initialise(database="news",
                     password="marshal",
                     host="localhost")
 
+equal_sign = '======================================================='
+hashtag_sign = '#######################################################'
+
+log_file = open('arquivo_log.txt', 'w')
+
 def popular_articles():
     list_articles = Articles.get_articles()
     return list_articles
 
 def display_popular_articles():
     lt_articles = popular_articles()
-    print("=======================================================")
-    print("|    OS 3 ARTIGOS MAIS POPULARES DE TODOS OS TEMPOS   |")
-    print("=======================================================")
+    main_article_title = 'OS 3 ARTIGOS MAIS POPULARES DE TODOS OS TEMPOS'
+    print(equal_sign)
+    print("|    "+ main_article_title +"   |")
+    print(equal_sign)
     for i in range(len(lt_articles)):
         print("| \"%s\" -- %s views  |" %(lt_articles[i][0], lt_articles[i][1]))
-    print("#######################################################")
+    print(hashtag_sign)
 
 
 def popular_authors():
@@ -30,12 +36,31 @@ def popular_authors():
 
 def display_popular_authors():
     lt_authors = popular_authors()
-    print("=========================================================")
-    print("| AUTORES DOS ARTIGOS MAIS POPULARES DE TODOS OS TEMPOS |")
-    print("=========================================================")
+    main_authors_title = 'AUTORES DOS ARTIGOS MAIS POPULARES DE TODOS OS TEMPOS'
+    print(equal_sign)
+    print("| "+ main_authors_title +" |")
+    print(equal_sign)
     for i in range(len(lt_authors)):
         print("\t\"%s\" -- %s views" %(lt_authors[i][0], lt_authors[i][1]))
-    print("#########################################################")
+    print(hashtag_sign)
+
+
+def erros_percentagem():
+    Erros.create_tb_estatus_erros()
+    Erros.create_tb_status_all()
+    Erros.create_tb_all_erros()
+    list_erros = Erros.select_erros_percentagem()
+    return list_erros
+
+def display_erros_percentagem():
+    lt_erros = erros_percentagem()
+    print(equal_sign)
+    print("| DIA QUE MAIS DE 1% DAS REQUISIÇÕES RESULTAM EM ERROS  |")
+    print(equal_sign)
+    for i in range (len(lt_erros)):
+        print ("%s -- %.2f %% errors" %(lt_erros[i][0], float(lt_erros[i][1])))
+    print(hashtag_sign)
 
 display_popular_articles()
 display_popular_authors()
+display_erros_percentagem()
--- erros.py
+++ erros.py
@@ -2,15 +2,43 @@ from database import CursorFromConnectionFromPool
 
 class Erros:
     @classmethod
-    def get_log(cls):
+    def create_tb_estatus_erros(cls):
         with CursorFromConnectionFromPool() as cursor:
-            cursor.execute('''select date(time), count(status) as num from log
-                              where status = '404 NOT FOUND' group by date(time)
+            cursor.execute('''create or replace view tb_status_erros as select
+                              to_char(date(time), 'Month DD, YYYY') as mes_dia_ano
+                              , count(status) as status_erros from log where
+                              status='404 NOT FOUND' group by date(time)
                               order by date(time)''')
-            log_data = cursor.fetchall()
-            return log_data
 
-# create or replace view tb_status_erros as select to_char(date(time), 'Month DD, YY') as mes_dia_ano, count(status) as status_all from log where status='404 NOT FOUND' group by date(time) order by date(time);
-# create or replace view tb_status_all as select to_char(date(time), 'Month DD, YY') as m_d_a, count(status) as status_erros from log group by date(time) order by date(time);
+    @classmethod
+    def create_tb_status_all(cls):
+        with CursorFromConnectionFromPool() as cursor:
+            cursor.execute('''create or replace view tb_status_all as select
+                              to_char(date(time), 'Month DD, YYYY') as m_d_a,
+                              count(status) as status_all from log group by
+                              date(time) order by date(time)''')
+
+    @classmethod
+    def create_tb_all_erros(cls):
+        with CursorFromConnectionFromPool() as cursor:
+            cursor.execute('''create or replace view tb_all_erros as select
+                              mes_dia_ano, status_all, status_erros
+                              from tb_status_all join tb_status_erros on
+                              tb_status_all.m_d_a = tb_status_erros.mes_dia_ano
+                              group by mes_dia_ano, status_all, status_erros
+                              order by mes_dia_ano''')
+
+    @classmethod
+    def select_erros_percentagem(cls):
+        with CursorFromConnectionFromPool() as cursor:
+            cursor.execute('''select mes_dia_ano,
+                              (cast(status_erros as float)/(status_all)*100) as
+                              porcentagem from tb_all_erros where
+                              (cast(status_erros as float8)/cast(status_all as float8)*100) > 1  order by mes_dia_ano''')
+            err_percentagem = cursor.fetchall()
+            return err_percentagem
+
+# create or replace view tb_status_erros as select to_char(date(time), 'Month DD, YY') as mes_dia_ano, count(status) as status_erros from log where status='404 NOT FOUND' group by date(time) order by date(time);
+# create or replace view tb_status_all as select to_char(date(time), 'Month DD, YY') as m_d_a, count(status) as status_all from log group by date(time) order by date(time);
 # create or replace view tb_all_erros as select mes_dia_ano, status_all, status_erros from tb_status_all join tb_status_erros on tb_status_all.m_d_a = tb_status_erros.mes_dia_ano group by mes_dia_ano, status_all, status_erros order by mes_dia_ano;
 # select mes_dia_ano, (cast(status_erros as float)/(status_all)*100) as porcentagem from tb_all_erros where (cast(status_erros as float8)/cast(status_all as float8)*100) > 1  order by mes_dia_ano;
